34483da65bceeec51beaa8fb78bc841316e15ef8
Junian Triajianto <author@junian.dev>
2025-01-18 15:37:02 +0700

    Finish form

--- linuxfoundation/introduction-to-nodejs/my-node-project/mock-srv/routes/confectionery/index.mjs
+++ linuxfoundation/introduction-to-nodejs/my-node-project/mock-srv/routes/confectionery/index.mjs
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 const data = [
     {
         id: "B1",
@@ -8,9 +7,12 @@ const data = [
         info: "Delicious overpriced chocolate.",
     },
 ];
-
-export default async (fastify) => {
-    fastify.get('/', async (req, res) => {
+export default async function (fastify, opts) {
+    fastify.get("/", async function (request, reply) {
         return data;
     });
-};
+    fastify.post("/", async function (request, reply) {
+        fastify.mockDataInsert(request, opts.prefix.slice(1), data);
+        return data;
+    });
+}
